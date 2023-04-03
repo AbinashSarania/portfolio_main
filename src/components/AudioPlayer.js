12939337ad2cfc2fe273b9b9068bce3730e1e6d0
AbinashSarania <102679085+AbinashSarania@users.noreply.github.com>
2023-04-03 23:51:55 +0530

    Update AudioPlayer.js

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -1,30 +1,29 @@
-import React, { useState, useRef, useEffect } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faVolumeMute, faVolumeUp } from "@fortawesome/free-solid-svg-icons";
+import React, { useState, useRef } from "react";
 import musicFile from "../assets/themesong.mp3";
+import { faVolumeMute, faVolumeUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const AudioPlayer = () => {
-    const [isMuted, setIsMuted] = useState(false);
+    const [isMuted, setIsMuted] = useState(true);
     const audioRef = useRef(null);
-
-    useEffect(() => {
-        audioRef.current.play();
-    }, []);
 
     const handleToggleMute = () => {
         const audio = audioRef.current;
-        setIsMuted(!isMuted);
-        audio.muted = !isMuted;
+
+        if (audio) {
+            audio.muted = !audio.muted;
+            setIsMuted(audio.muted);
+        }
     };
 
     return (
-        <div>
-            <audio ref={audioRef} autoPlay loop>
+        <div className="flex flex-col items-center justify-center h-full">
+            <audio ref={audioRef} autoPlay loop muted={isMuted}>
                 <source src={musicFile} type="audio/mpeg" />
             </audio>
             <button
                 onClick={handleToggleMute}
-                className="bg-gradient-to-r from-blue-500 to-indigo-600 hover:bg-blue-700 text-white font-bold py-2 px-2.5 rounded"
+                className="bg-gradient-to-r from-green-400 to-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 sm:py-1 sm:px-2 rounded"
             >
                 {isMuted ? (
                     <FontAwesomeIcon icon={faVolumeMute} className="mr-1" />
@@ -33,6 +32,7 @@ const AudioPlayer = () => {
                 )}
                 {isMuted ? "Unmute" : "Mute"}
             </button>
+
         </div>
     );
 };
